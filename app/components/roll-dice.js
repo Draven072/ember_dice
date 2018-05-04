347bda8498745656572360f10b3526c59c0d69a0
Draven072 <mdmitry072@gmail.com>
2018-05-04 19:32:40 +0300

    Added actions for Roll Dice button

--- app/components/roll-dice.js
+++ app/components/roll-dice.js
@@ -4,14 +4,16 @@ export default Component.extend({
     rollName: '',
     numberOfDice: 1,
     numberOfSides: 6,
+
+    actions: {
+      triggerRoll() {
+        alert(`Rolling ${this.numberOfDice}D${this.numberOfSides} as "${this.rollName}"`);
+        return false;
+      }
+    },
+
     didRender() {
         Materialize.updateTextFields();
     }
-});
 
-actions: {
-   triggerRoll () {
-       alert(`Rolling ${this.numberOfDice}D${this.numberOfSides} as "${this.rollName}"`);
-       return false;
-   }
-}
+});
